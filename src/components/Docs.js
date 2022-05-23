6a9c6f63048325b2e27ee63f7e9da27119d0c996
Molly <mojonesycreative@gmail.com>
2022-05-22 21:25:13 -0400

    load and display docs

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ModalComponent from "./Modal";
+import { addDoc, collection, onSnapshot } from 'firebase/firestore';
+import './Docs.css';
 
-export default function Docs() {
+export default function Docs({ database }) {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const [title, setTitle] = useState('');
+    const [docsData, setDocsData] = useState([]);
+    
+    const collectionRef = collection(database, 'docsData');
+    const getData = () => {
+        onSnapshot(collectionRef, (data) => {
+            setDocsData(data.docs.map((doc) => {
+                return { ...doc.data(), id: doc.id }
+            }))
+        })
+    }
+
+    // If 'isMounted.current' is true, return nothing - prevents concurrent rendering.
+    //  Set 'isMounted.current to true and call getData().
+    const isMounted = useRef();
+    useEffect(() => {
+        if(isMounted.current) {
+            return
+        }
+        
+        isMounted.current = true;
+        getData();
+    }, []);
+
+    const addData = () => {
+        addDoc(collectionRef, {
+            title: title
+        })
+        .then(() => {
+            alert('Data Added')
+            handleClose()
+        })
+        .catch(() => {
+            alert('Cannot add data')
+        });
+    }
 
     return (
-        <div>
+        <>
             <h1>Docs Clone</h1>
 
             <button 
@@ -15,11 +54,22 @@ export default function Docs() {
             >
                 Add a Document
             </button>
-
+        <div className='grid-main'>
+            {docsData.map((doc) => {
+                return (
+                    <div className='grid-child'>
+                        <p>{doc.title}</p>
+                    </div>
+                );
+            })}
+        </div>
             <ModalComponent 
              open={open} 
-             setOpen={setOpen} 
+             setOpen={setOpen}
+             title={title} 
+             setTitle={setTitle}
+             addData={addData}
             />
-        </div>
+        </>
     );
 }
